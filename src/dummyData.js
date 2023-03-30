89618b01c439e5289a4ac303b31b6ddda097d0b7
unknown <desphixs@gmail.com>
2023-03-30 09:11:24 -0700

    first commit

---
+++ src/dummyData.js
@@ -0,0 +1,83 @@
+
+export const Users = [
+    {
+        id:1,
+        profilePicture: "assets/person/1.jpg",
+        username: "John Doe"
+    },
+
+    {
+        id:2,
+        profilePicture: "assets/person/2.jpg",
+        username: "Ben Carson"
+    },
+
+    {
+        id:3,
+        profilePicture: "assets/person/3.jpg",
+        username: "Destiny Franks"
+    },
+
+    {
+        id:4,
+        profilePicture: "assets/person/4.jpg",
+        username: "Davy Jones"
+    },
+    {
+        id:5,
+        profilePicture: "assets/person/5.jpg",
+        username: "Destiny Franks"
+    },
+]
+
+
+export const Posts = [
+    {
+        id:1,
+        caption: "My first Post",
+        photo: "assets/post/1.jpg",
+        date: "15 Days Ago",
+        userId: 2,
+        like: 2,
+        comment: 3
+    },
+
+    {
+        id:2,
+        caption: "My Second Post",
+        photo: "assets/post/2.jpg",
+        date: "1 Sec Ago",
+        userId: 2,
+        like: 22,
+        comment: 11
+    },
+
+    {
+        id:3,
+        photo: "assets/post/3.jpg",
+        date: "10 Months Ago",
+        userId: 3,
+        like: 22,
+        comment: 31
+    },
+
+    {
+        id:4,
+        caption: "My Fourth Post",
+        photo: "assets/post/4.jpg",
+        date: "3 Days Ago",
+        userId: 4,
+        like: 5122,
+        comment: 401
+    },
+    
+    {
+        id:5,
+        caption: "My Fifth Post",
+        photo: "assets/post/5.jpg",
+        date: "3 Days Ago",
+        userId: 1,
+        like: 52,
+        comment: 43
+    },
+]
